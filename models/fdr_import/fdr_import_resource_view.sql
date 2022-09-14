1137cc7613c5e3db22a13b15dd61d4319fd3f753
Marc Dutoo <marc.dutoo@gmail.com>
2022-09-14 17:02:33 +0200

    Some metrics over import results, to be used in Superset.

--- models/fdr_import/fdr_import_resource_view.sql
+++ models/fdr_import/fdr_import_resource_view.sql
@@ -1,5 +1,6 @@
 {#
-Used to define fal on it to publish it
+Provides metrics over import results.
+Also used to define fal on it to publish it.
 
     schema=source('fdr_import', 'fdr_import_resource').schema
 NOO "custom schema" creates ex. appuiscommuns_appuiscommuns schema unless generate_schema_name() changed :
@@ -12,4 +13,14 @@ https://docs.getdbt.com/docs/building-a-dbt-project/building-models/using-custom
   )
 }}
 
-select * from {{ source('fdr_import', 'fdr_import_resource') }}
+with import_resource as (
+  select * from {{ source('fdr_import', 'fdr_import_resource') }}
+
+), with_metrics as (
+    SELECT *,
+        (case when status = 'success' then 1 else 0 end ) As success,
+        (case when status = 'skipped' then 1 else 0 end ) As skipped,
+        (case when status = 'error' then 1 else 0 end ) As error
+    from import_resource
+)
+select * from with_metrics
