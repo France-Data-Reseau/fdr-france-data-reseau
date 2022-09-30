948d2f76cbfa2c8f70b0e981756f5841396e535b
Marc Dutoo <marc.dutoo@gmail.com>
2022-09-30 13:30:47 +0200

    import automatisation - patch env vars set when run by Nifi (else Docker permission denied)

--- fdr_reset_run_import_dbt_all.sh
+++ fdr_reset_run_import_dbt_all.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-. ../../dbt-env/bin/activate
 cd ../fdr-france-data-reseau
-set -a ; source env.prod_stellio ; set +a
-echo check : FDR_SYNC_POSTGRES_DATABASE=$FDR_SYNC_POSTGRES_DATABASE
 rm -rf import_state.json
-fal run --target prod --before
-dbt run --target prod
-cd ../fdr-eaupotable
-dbt run --target prod
+./fdr_run_import_dbt_all.sh
--- fdr_run_import_dbt_all.sh
+++ fdr_run_import_dbt_all.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 . ../../dbt-env/bin/activate
 cd ../fdr-france-data-reseau
+
+# patch env vars set when run by Nifi :
+# (else error permission denied when running Docker for ogr2ogr)
+set -a
+unset SUDO_USER
+unset SUDO_UID
+unset SUDO_GID
+set +a
+
 set -a ; source env.prod_stellio ; set +a
-echo check : FDR_SYNC_POSTGRES_DATABASE=$FDR_SYNC_POSTGRES_DATABASE
+echo check :
+FDR_SYNC_POSTGRES_DATABASE=$FDR_SYNC_POSTGRES_DATABASE
+SUDO_USER=SUDO_USER
 fal run --target prod --before
 dbt run --target prod
 cd ../fdr-eaupotable
